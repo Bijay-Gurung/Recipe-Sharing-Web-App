d107f6040f8565497068f5e3dd490fa6451d4af7
Bijay-Gurung <stha4580@gmail.com>
2024-08-02 20:00:57 +0545

    Creating the nav and body of home page

--- src/View/Home.js
+++ src/View/Home.js
@@ -1,11 +1,56 @@
 import React from 'react';
 import Nav from '../Component/Nav';
+import './Home.css';
+
+function GroceryList(){
+    return(
+        <div className='GroceryList'>
+            <div className='Left'></div>
+            <div className='Right'>
+                <h1>Grocery List</h1> {/* Cherry Bomb */}
+                <p>Look for your Grocery Item</p>
+                <button>Check</button>
+            </div>
+        </div>
+    );
+}
+
+function Categories(){
+    return(
+        <div className='Categories'>
+            <div className='breakfast'>
+                <div className='bimage'></div>
+                <h3>Breakfast</h3>
+                <button>Click Here</button>
+            </div>
+
+            <div className='meal'>
+                <div className='mimage'></div>
+                <h3>Meal</h3>
+                <button>Click Here</button>
+            </div>
+
+            <div className='dinner'>
+                <div className='dimage'></div>
+                <h3>Dinner</h3>
+                <button>Click Here</button>
+            </div>
+
+            <div className='dessert'>
+                <div className='deimage'></div>
+                <h3>Dessert</h3>
+                <button>Click Here</button>
+            </div>
+        </div>
+    )
+}
 
 export default function Home() {
     return (
         <div>
             <Nav />
-            <h1>Hello World</h1>
+            <GroceryList />
+            <Categories />
         </div>
     );
 }
