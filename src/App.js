cd9532507c358eb23f9bc4be270cdb441925b0e0
Bijay-Gurung <stha4580@gmail.com>
2024-08-04 18:45:21 +0545

    Adding router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from "react";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Signup from "./View/Signup";
-// import Login from "./View/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Signup from "./View/Signup";
+import Login from "./View/Login";
 import Home from './View/Home';
+import Nav from '../Component/Nav';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />
+    },
+    {
+      path: '/',
+      element: <About />
+    },
+  ])
+
   return (
-    // <div>
-    //   <BrowserRouter>
-    //   <Routes>
-    //     <Route index element = {<Signup />} />
-    //     <Route path="./Pages/Login.js" element={<Login />} />
-    //   </Routes>
-    //   </BrowserRouter>
-    // </div>
     <>
-    <Home />
+    <Home/>
+    <RouterProvider router={router} />
     </>
   );
 }
