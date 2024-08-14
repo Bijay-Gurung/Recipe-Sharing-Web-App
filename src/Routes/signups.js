b55c1358287d74edce246d6caba1c678fa0da849
Bijay-Gurung <stha4580@gmail.com>
2024-08-14 19:22:48 +0545

    Adding backend for signup

---
+++ src/Routes/signups.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const Signup = require('../models/signup');
+const router = express.Router();
+
+
+router.post('/', async (req,res) => {
+    try{
+        const signup = new Signup(req.body);
+        await signup.save();
+        console.log('Singup saved:', signup);
+        res.status(201).json(signup);
+    }catch (error){
+        console.log('Error while signup:', error);
+        res.status(400).json({message: 'error while signup', error: error.message});
+    }
+});
+
+module.exports = router;
+
+router.get('/', async (req,res) => {
+    try{
+        const signups = await Signup.find();
+        res.json(signups);
+    }catch (error){
+        console.error('error retrieving signup data:', error);
+        res.status(500).json({message: 'Error retrieving signup data', error: error.message});
+    }
+});
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const recipesRouter = require('./Routes/recipes');
+const signupRouter = require('./Routes/signups');
 const cors = require('cors');
 
 const app = express();
@@ -10,10 +11,11 @@ const port = 4000;
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "http://localhost:3000/",
 }))
 // Routes
 app.use('/recipes', recipesRouter);
+app.use('/signups', signupRouter);
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/recipe-sharing')
