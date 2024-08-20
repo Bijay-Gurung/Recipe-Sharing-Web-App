970447cdf9338d0ce86c85c94baea5e0ad35bb3e
Bijay-Gurung <stha4580@gmail.com>
2024-08-20 08:25:42 +0545

    trying to make about us page

--- src/View/About.js
+++ src/View/About.js
@@ -1,9 +1,31 @@
 import React from "react";
+import Nav from "../Component/Nav";
+import Footer from "../Component/Footer";
+import './About.css';
+
+function Content(){
+    return(
+        <div className="content">
+            <div className="image"></div>
+            <div className="content1">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </div>
+            <div className="content2">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </div>
+            <div className="content3">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            </div>
+        </div>
+    )
+}
 
 export default function About(){
     return(
         <>
-        <p>HI</p>
+        <Nav />
+        <Content />
+        <Footer />
         </>
     );
 }
