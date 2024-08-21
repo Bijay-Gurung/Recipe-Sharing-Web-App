1bd4068c6c36e52813296c6f8e5f5da771790caa
Bijay-Gurung <stha4580@gmail.com>
2024-08-21 17:09:27 +0545

    Creating the frontend of Create Recipes page

--- src/View/AdminCreate.js
+++ src/View/AdminCreate.js
@@ -25,15 +25,20 @@ function Create(){
         <div className="createRecipe">
             <h1>New Recipe</h1>
             <form onSubmit={handleSubmit}>
-                <input type="text" id="recipe" name="recipe" placeholder="Recipe Name" onChange={(e) => setRecipe(e.target.value)} required />
-                <input type="text" id="author" name="author" placeholder="Author Name" onChange={(e) => setAuthor(e.target.value)} required />
-                <input type="date" id="date" name="date" placeholder="Date" onChange={(e) => setDate(e.target.value)} required />
-                <textarea rows='5' cols='24' id="nutInfo" name="nutInfo" placeholder="Add Nutritional Info" onChange={(e) => setNutInfo(e.target.value)} required></textarea>
-                <textarea rows='5' cos='24' id="process" name="process" placeholder="Add the process of recipe" onChange={(e) => setProcess(e.target.value)} required></textarea>
-                <input type="file" id="recipeImage" name="recipeImage" placeholder="Upload Recipe Image" onChange={(e) => setImage(e.target.value)} required />
-                <textarea rows='5' cos='24' id="details" name="details" placeholder="Write your recipe details here" onChange={(e) => setDetails(e.target.value)} required></textarea>
-                <textarea rows='5' cols='24' id="ingredient" name="ingredient" placeholder="Add Ingredient" onChange={(e) => setIngredient(e.target.value)} required></textarea>
-                <button type="submit">Create</button>
+                <div className="left">
+                    <input type="text" id="recipe" name="recipe" placeholder="Recipe Name" onChange={(e) => setRecipe(e.target.value)} required /><br/>
+                    <input type="text" id="author" name="author" placeholder="Author Name" onChange={(e) => setAuthor(e.target.value)} required /><br/>
+                    <input type="date" id="date" name="date" placeholder="Date" onChange={(e) => setDate(e.target.value)} required /><br/>
+                    <textarea rows='5' cols='29' id="nutInfo" name="nutInfo" placeholder="Add Nutritional Info" onChange={(e) => setNutInfo(e.target.value)} required></textarea><br/>
+                    <textarea rows='5' cols='29' id="process" name="process" placeholder="Add the process of recipe" onChange={(e) => setProcess(e.target.value)} required></textarea><br/>
+                </div>
+
+                <div className="right">
+                    <input type="file" id="recipeImage" name="recipeImage" placeholder="Upload Recipe Image" onChange={(e) => setImage(e.target.value)} required /><br/>
+                    <textarea rows='5' cols='29' id="details" name="details" placeholder="Write your recipe details here" onChange={(e) => setDetails(e.target.value)} required></textarea><br/>
+                    <textarea rows='5' cols='29' id="ingredient" name="ingredient" placeholder="Add Ingredient" onChange={(e) => setIngredient(e.target.value)} required></textarea><br/>
+                    <button type="submit">Create</button>
+                </div>
             </form>
         </div>
     );
