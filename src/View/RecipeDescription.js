f3c9f88dbac4df7a6e4cdf265910f19a9b0bc594
Bijay-Gurung <stha4580@gmail.com>
2024-10-21 20:50:07 +0545

    Completing the UI of recipe description

--- src/View/RecipeDescription.js
+++ src/View/RecipeDescription.js
@@ -22,39 +22,71 @@ function Content(){
             </div>
 
             <div className="nutritionalInfo">
-                <div className="left">
-                    <ul>
-                        <li>Serving size 1 slice (72g/2.6oz)</li>
-                        <li>Serving per container 3</li>
-                        <li>Amount per serving (% daily values)</li>
-                    </ul>
-                </div>
+            <h2>Nutritional Info</h2>
+                <div className="content">
+                    <div className="left">
+                        <div className="section">
+                            <ul>
+                                <li>Serving size 1 slice (72g/2.6oz)</li>
+                                <li>Serving per container 3</li>
+                                <li>Amount per serving (% daily values)</li>
+                            </ul>
+                        </div>
+                    </div>
+                    
+                    <div className="right">
+                        <div className="section">
+                            <div className="c1">
+                                <ul>
+                                    <li>Calories 270</li>
+                                    <li>Calories from fat 110</li>
+                                    <li>Total fat 12g (18%)</li>
+                                    <li>Saturated fat 7g (34%)</li>
+                                </ul>
+                            </div>
 
-                <div className="right">
-                    <div className="section">
-                        <ul>
-                            <li>Calories 270</li>
-                            <li>Calories from fat 110</li>
-                            <li>Total fat 12g (18%)</li>
-                            <li>Saturated fat 7g (34%)</li>
-                        </ul>
+                            <div className="c2">
+                                <ul>
+                                    <li>Trans fat 0g</li>
+                                    <li>Cholesterol 35mg (11%)</li>
+                                    <li>Sodium 420mg (18%)</li>
+                                    <li>Total Carbohydrate 31g (10%)</li>
+                                </ul>
+                            </div>
+
+                            <div className="c3">
+                                <ul>
+                                    <li>Dietary fiber 2g (9%)</li>
+                                    <li>Sugar 2g</li>
+                                    <li>Protien 10g</li>
+                                </ul>
+                            </div>
+                            
+                        </div>
                     </div>
                 </div>
             </div>
 
             <div className="ingredient">
-                <p>Organic Bread (organic wheat flour, water, yeast & salt) butter (pasteurized rbst free cream [milk], salt), fresh garlic, roasted
-                garlic, fresh basil, whole milk mozzarella (cultured pasteurized whole milk, salt, enzymes)</p>
+                <h2>Ingredient</h2>
+                <div className="content">
+                    <p>Organic Bread (organic wheat flour, water, yeast & salt) butter (pasteurized rbst free cream [milk], salt), fresh garlic, roasted
+                        garlic, fresh basil, whole milk mozzarella (cultured pasteurized whole milk, salt, enzymes)</p>
+                </div>
+                
             </div>
 
-            <div className="Processes">
-                <ul>
-                    <li>Preheat oven to 450 degree.</li>
-                    <li>Remove bread from packaging and place on baking sheet.</li>
-                    <li>When oven is hot, place garlic bread on middle rack of oven and bake until cheese is just melted and
+            <div className="processes">
+                <h2>Processes</h2>
+                <div className="content">
+                    <ol>
+                        <li>Preheat oven to 450 degree.</li>
+                        <li>Remove bread from packaging and place on baking sheet.</li>
+                        <li>When oven is hot, place garlic bread on middle rack of oven and bake until cheese is just melted and
                         started to brown and bubble, approximately 6 to 8 minutes.</li>
-                    <li>Caution: cheese will be hot, let cool slightly.</li>
-                </ul>
+                        <li>Caution: cheese will be hot, let cool slightly.</li>
+                    </ol>
+                </div>
             </div>
         </div>
     );
